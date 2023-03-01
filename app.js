e5dad73fb70a0736c8d6c5a5a87cb70c2e93c248
Richard Rino <richardvrino@gmail.com>
2023-03-01 01:13:58 -0600

    Add the ability to display numbers and to perform an operation on two numbers

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let displayValue = "";
 let currentOperator = null;
-let num1 = 0;
-let num2 = 0;
+let num1 = null;
+let num2 = null;
+let result = null;
 
 function add(num1, num2) {
   return num1 + num2;
@@ -28,6 +29,45 @@ const numberButtons = document.querySelectorAll(".number");
 
 numberButtons.forEach((item) => {
   item.addEventListener("click", (e) => {
-    displayValue = displayValue + e.target.textContent;
+    const calculatorScreen = document.querySelector(".calculator__screen");
+    if (currentOperator === null) {
+      displayValue = displayValue + e.target.textContent;
+      num1 = displayValue;
+    } else if (currentOperator && num2 === null) {
+      displayValue = "";
+      displayValue = displayValue + e.target.textContent;
+      calculatorScreen.textContent = displayValue;
+      num2 = displayValue;
+    } else {
+      displayValue = displayValue + e.target.textContent;
+      calculatorScreen.textContent = displayValue;
+      num2 = displayValue;
+    }
+
+    if (currentOperator !== null && displayValue !== "") {
+    }
+    calculatorScreen.textContent = displayValue;
   });
+});
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    currentOperator = e.target.dataset.operator;
+  });
+});
+
+const equalsButton = document.querySelector(".equals");
+
+equalsButton.addEventListener("click", (e) => {
+  const result = operate(
+    this[currentOperator],
+    Number(num1),
+    Number(num2)
+  ).toString();
+  const calculatorScreen = document.querySelector(".calculator__screen");
+  calculatorScreen.textContent = result;
+  num1 = result;
+  num2 = null;
 });
