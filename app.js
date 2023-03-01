750d57c25ea483b3e4e72a00603ee49d7fd44535
Richard Rino <richardvrino@gmail.com>
2023-02-28 23:26:49 -0600

    Add the ability to store the number of the button that is pressed

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const add = function (num1, num2) {
+let displayValue = "";
+let currentOperator = null;
+let num1 = 0;
+let num2 = 0;
+
+function add(num1, num2) {
   return num1 + num2;
-};
+}
 
-const subtract = function (num1, num2) {
+function subtract(num1, num2) {
   return num1 - num2;
-};
+}
 
-const multiply = function (num1, num2) {
+function multiply(num1, num2) {
   return num1 * num2;
-};
+}
 
-const divide = function (num1, num2) {
+function divide(num1, num2) {
   return num1 / num2;
-};
+}
 
-const operate = function (num1, num2, operation) {
-  let result = operation(num1, num2);
+function operate(operator, num1, num2) {
+  let result = operator(num1, num2);
   return result;
-};
+}
+
+const numberButtons = document.querySelectorAll(".number");
+
+numberButtons.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    displayValue = displayValue + e.target.textContent;
+  });
+});
